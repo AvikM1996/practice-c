c0533c91154f62434e6badf52de6b10f97b77af4
Avishek <avishek.maity@trpglobal.uk>
2025-07-09 18:39:11 +0530

    array

--- Array/array.cpp
+++ Array/array.cpp
@@ -99,29 +99,146 @@
 // }
 
 // Binary search
+// int main()
+// {
+//     int a[] = {1, 2, 3, 4, 5};
+//     int size = sizeof(a) / sizeof(a[0]);
+//     int left = 0, right = size - 1, key,mid;
+
+//     std ::cout << "Enter key";
+//     std::cin >> key;
+
+//     while (left <= right)
+//     {
+//         mid = (left + right) / 2;
+//         if (key == a[mid])
+//         {
+//             std::cout << mid;
+//             return 0;
+//         }
+//         else if (key < a[mid])
+//         {
+//           right=mid-1;
+//         }else{
+//             left=mid+1;
+//         }
+//     }
+//     std:: cout<< "Not found";
+// }
+
+// int main()
+// {
+//     int a, j, i;
+//     std::cout << "ender a number";
+//     std ::cin >> a;
+//     for (i = 0; i < a; i++)
+//     {
+//         for (j = 0; j < a; j++)
+//         {
+//             std::cout << i << j << std::endl;
+//         }
+//     }
+//     return 0;
+// }
+
+// int main()
+// {
+//     int n = 9;
+//     int a[] = {2, 3, 4, 5, 9, 10, 11, 12, 13, 22};
+//     int length = sizeof(a) / sizeof(a[0]);
+
+//     int low = 0;
+//     int high = 9;
+
+//     int key;
+//     std::cout << "Enter key : ";
+//     std::cin >> key;
+//     while (low <= high)
+//     {
+//         int mid = (low + high) / 2;
+//         if (key == a[mid])
+//         {
+//             std::cout << "found it at index : " << mid;
+//             return 0;
+//         }
+//         else if (key < a[mid])
+//         {
+//             high = mid - 1;
+//         }
+//         else
+//         {
+//             low = mid + 1;
+//         }
+//     }
+
+//     std::cout << "not found";
+// }
+
+// int main()
+// {
+//     int a[] = {2, 4, 5, 7, 23, 45, 65};
+//     int low = 0;
+//     int high = 7;
+//     int mid;
+//     int key;
+//     std::cout << "Enter key : ";
+//     std::cin >> key;
+
+//     while (low <= high)
+//     {
+//         mid = (low + high) / 2;
+//         if (key == a[mid])
+//         {
+//             std::cout << "found it at index : " << mid;
+//             return 0;
+//         }
+//         else if (key < a[mid])
+//         {
+//             high = mid - 1;
+//         }
+//         else
+//         {
+//             low = mid + 1;
+//         }
+//     }
+// }
+
+// Nasted Loop
+
+// int main()
+// {
+//     int num, i;
+
+//     std::cout << "enter a number : ";
+//     std::cin >> num;
+
+//     for (i = 1; i <= num; i++)
+//     {
+//     //   std::cout <<"Step : "<< i << std::endl;
+//       for (int j = 1; j <= i; j++){
+//         std::cout << j << std::endl;
+//       }
+
+//     }
+//     return 0;
+
+// }
+
+// squre pattern
 int main()
 {
-    int a[] = {1, 2, 3, 4, 5};
-    int size = sizeof(a) / sizeof(a[0]);
-    int left = 0, right = size - 1, key,mid;
-
-    std ::cout << "Enter key";
+    int key, count = 1;
+    std::cout << "enter a number : ";
     std::cin >> key;
-
-    while (left <= right)
+    for (int i = 1; i <= key; i++)
     {
-        mid = (left + right) / 2;
-        if (key == a[mid])
+        for (int j = 1; j <= key; j++)
         {
-            std::cout << mid;
-            return 0;
+            std::cout << count<<" ";
+            count++;
         }
-        else if (key < a[mid])
-        {
-          right=mid-1;  
-        }else{
-            left=mid+1;
-        }
+        std::cout << std::endl;
     }
-    std:: cout<< "Not found";
+
+    return 0;
 }
